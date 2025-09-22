0addba4061b195693957140a48fddd1da3e5b652
bilgisen <hbkarabey@gmail.com>
2025-09-22 11:11:13 +0300

    toc

--- scripts/generate-toc.sh
+++ scripts/generate-toc.sh
@@ -13,11 +13,24 @@ OUTPUT_FILE="$2"
 # --- jq ile Sağlam TOC Oluşturma ---
 TOC_BODY=$(jq -r '
   def escape_html: gsub("&"; "&amp;") | gsub("<"; "&lt;") | gsub(">"; "&gt;") | gsub("\""; "&quot;");
-  def pad_num: tostring | ("000" + .) | .[-3:];
+  def pad_num: (. + 2) | tostring | ("000" + .) | .[-3:];  # +2 ekleyerek ch003'ten başlat
+
+  # Dil kodlarına göre TOC başlıkları
+  def get_toc_title:
+    if . == "tr" then "İçindekiler"
+    elif . == "en" then "Table of Contents"
+    elif . == "fr" then "Table des matières"
+    elif . == "de" then "Inhaltsverzeichnis"
+    elif . == "es" then "Índice"
+    elif . == "ru" then "Содержание"
+    elif . == "zh" then "目录"
+    elif . == "ar" then "جدول المحتويات"
+    else "Table of Contents"
+    end;
 
   (.book.language // "en") as $lang
-  | (if $lang == "tr" then "İçindekiler" else "Table of Contents" end) as $title
-  | "<nav epub:type=\"toc\" id=\"toc\"><h1>\($title)</h1><ol>" + (
+  | ($lang | get_toc_title) as $title
+  | "<nav epub:type=\"toc\" id=\"toc\"><h1>\($title)</h1><ul>" + (
       .book.chapters
       | sort_by(.order // 0)
       | map(
@@ -25,7 +38,7 @@ TOC_BODY=$(jq -r '
           (.title | escape_html) +
           "</a></li>"
         ) | join("")
-    ) + "</ol></nav>"
+    ) + "</ul></nav>"
 ' "$PAYLOAD_FILE")
 
 # --- XHTML Dosyasını Oluşturma ---
@@ -33,7 +46,19 @@ TOC_BODY=$(jq -r '
   echo '<?xml version="1.0" encoding="UTF-8"?>'
   echo '<!DOCTYPE html>'
   echo '<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">'
-  echo '<head><meta charset="utf-8"/><title>İçindekiler</title></head>'
+  # TOC başlığını kitap diline göre ayarla
+  TOC_TITLE=$(jq -r '
+    if .book.language == "tr" then "İçindekiler"
+    elif .book.language == "en" then "Table of Contents"
+    elif .book.language == "fr" then "Table des matières"
+    elif .book.language == "de" then "Inhaltsverzeichnis"
+    elif .book.language == "es" then "Índice"
+    elif .book.language == "ru" then "Содержание"
+    elif .book.language == "zh" then "目录"
+    elif .book.language == "ar" then "جدول المحتويات"
+    else "Table of Contents"
+    end' "$PAYLOAD_FILE")
+  echo '<head><meta charset="utf-8"/><title>'"$TOC_TITLE"'</title></head>'
   echo '<body>'
   echo "$TOC_BODY"
   echo '</body></html>'
